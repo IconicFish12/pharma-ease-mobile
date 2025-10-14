b82b192ec205c49fd9caaffb8a443478ac4f8287
Ibnu Syawal Aliefian <superglidingogre0571@gmail.com>
2025-10-14 14:32:11 +0700

    [ADD]: Adding Data Repository

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_course_fp/views/home.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,8 +9,33 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  // @override
+  // void initState() {
+  //   super.initState();
+
+  //   startSplashScreen();
+  // }
+
+  // startSplashScreen() async {
+  //   var duration = const Duration(seconds: 5);
+
+  //   return Future.delayed(duration, () {
+  //     Navigator.pushReplacement(
+  //       context,
+  //       MaterialPageRoute(builder: (context) => HomePage()),
+  //     );
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [],
+      ),
+    );
   }
 }
