3cb9fe9e21c077be84d6c0dbbccb05d187a1de74
aprilHardinata <aprilhardihardi439@gmail.com>
2026-01-05 11:07:39 +0700

    Fix token issue secure storage

--- lib/data/provider/activity_log_provider.dart
+++ lib/data/provider/activity_log_provider.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart'; 
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import '../../config/config.dart';
 import '../../data/model/activity_log_model.dart';
