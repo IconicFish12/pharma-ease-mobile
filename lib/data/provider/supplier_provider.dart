b49af6c2962bd5e4e82f8ea95631d7ac743970e3
Ibnu Syawal Aliefian <superglidingogre0571@gmail.com>
2026-01-05 20:12:24 +0700

    [CHANGE]: Assigning Providers to main file

---
+++ lib/data/provider/supplier_provider.dart
@@ -0,0 +1,6 @@
+import 'package:mobile_course_fp/data/model/supplier_model.dart';
+import 'package:mobile_course_fp/data/provider/provider.dart';
+
+class SupplierProvider extends Provider<Datum>{
+  SupplierProvider(super.repository);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,18 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:mobile_course_fp/config/config.dart';
 import 'package:mobile_course_fp/data/provider/activity_log_provider.dart';
 import 'package:mobile_course_fp/data/provider/auth_provider.dart';
+import 'package:mobile_course_fp/data/provider/medicine_category_provider.dart';
+import 'package:mobile_course_fp/data/provider/medicine_order_provider.dart';
+import 'package:mobile_course_fp/data/provider/medicine_provider.dart';
+import 'package:mobile_course_fp/data/provider/supplier_provider.dart';
+import 'package:mobile_course_fp/data/provider/user_provider.dart';
 import 'package:mobile_course_fp/data/repository/auth_repository.dart';
+import 'package:mobile_course_fp/data/repository/medicine_category_repository.dart';
+import 'package:mobile_course_fp/data/repository/medicine_order_repository.dart';
+import 'package:mobile_course_fp/data/repository/medicine_repository.dart';
 import 'package:mobile_course_fp/data/repository/service/token_service.dart';
+import 'package:mobile_course_fp/data/repository/supplier_repository.dart';
+import 'package:mobile_course_fp/data/repository/user_repository.dart';
 import 'package:mobile_course_fp/firebase_options.dart';
 import 'package:mobile_course_fp/views/activity-log/activity_log.dart';
 import 'package:mobile_course_fp/views/auth/forgot_password.dart';
@@ -30,10 +40,9 @@ import 'package:mobile_course_fp/views/splash_screen.dart';
 import 'package:mobile_course_fp/views/suppliers/View/supplier_list.dart';
 import 'package:mobile_course_fp/views/users/profile_page.dart';
 import 'package:mobile_course_fp/views/users/user_management_page.dart';
-import 'package:provider/provider.dart';
-// import vm
 import 'package:mobile_course_fp/views/suppliers/ViewModel/supplier_viewmodel.dart';
 import 'package:mobile_course_fp/views/medicine/medicine-category/ViewModel/medicine_category_viewmodel.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -61,9 +70,13 @@ void main() async {
       providers: [
         ChangeNotifierProvider(create: (_) => SupplierViewModel()),
         ChangeNotifierProvider(create: (_) => MedicineCategoryViewmodel()),
-        
         ChangeNotifierProvider(create: (_) => AuthProvider(authRepository, tokenService)),
         ChangeNotifierProvider(create: (_) => ActivityLogProvider(tokenService)),
+        ChangeNotifierProvider(create: (_) => MedicineProvider(MedicineRepository(tokenService))),
+        ChangeNotifierProvider(create: (_) => MedicineOrderProvider(MedicineOrderRepository(tokenService))),
+        ChangeNotifierProvider(create: (_) => MedicineCategoryProvider(MedicineCategoryRepository(tokenService))),
+        ChangeNotifierProvider(create: (_) => SupplierProvider(SupplierRepository(tokenService))),
+        ChangeNotifierProvider(create: (_) => UserProvider(UserRepository(tokenService))),
       ],
       child: const MyApp(),
     ),
